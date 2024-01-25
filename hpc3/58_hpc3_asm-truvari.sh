628724dacbeb8d87af4372312bb3f345b62300a4
jyw-atgithub <jenyuw@uci.edu>
2024-01-25 12:01:59 -0800

    status

--- hpc3/58_hpc3_asm-truvari.sh
+++ hpc3/58_hpc3_asm-truvari.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-# in the interactive mode. 
+#SBATCH --job-name=asm-truvari    ## Name of the job.
+#SBATCH -A jje_lab       ## account to charge
+#SBATCH -p highmem       ## partition/queue name
+#SBATCH --array=1      ## number of tasks to launch (wc -l prefixes.txt)
+#SBATCH --cpus-per-task=8   ## number of cores the job needs-x map-ont
+#SBATCH --mem-per-cpu=10G     # requesting memory per CPUe. 
 source ~/.bashrc
 
 ref_genome="/dfs7/jje/jenyuw/SV-project-temp/reference/dmel-all-chromosome-r6.49.fasta"
 SVs="/dfs7/jje/jenyuw/SV-project-temp/result/SVs"
 merged_SVs="/dfs7/jje/jenyuw/SV-project-temp/result/merged_SVs"
+con_SVs="/dfs7/jje/jenyuw/SV-project-temp/result/consensus_SVs"
+nT=$SLURM_CPUS_PER_TASK
 
-
-#parallel -j 12 --progress bcftools index -f -t {} ::: ${SVs}/*.svimASM.filtered.vcf.gz
-bcftools merge -m none ${SVs}/*.svimASM.filtered.vcf.gz -O z -o - |\
-bcftools sort --max-mem 2G -O z -o - > ${merged_SVs}/merge.svimASM.sort.vcf.gz
-bcftools index -f -t ${merged_SVs}/merge.svimASM.sort.vcf.gz
+bcftools merge -m none --threads ${nT} ${con_SVs}/*.asm-2.tru_con.sort.vcf.gz -O z -o - |\
+bcftools sort --max-mem 4G -O z -o - > ${merged_SVs}/merge.asm-2.sort.vcf.gz
+bcftools index -f -t ${merged_SVs}/merge.asm-2.sort.vcf.gz
 
 module load python/3.10.2
 
 truvari collapse --sizemax 200000000 -k common \
--i ${merged_SVs}/merge.svimASM.sort.vcf.gz \
--c ${merged_SVs}/truvari.svimASM.collapse.vcf.gz -f ${ref_genome} |\
-bcftools sort --max-mem 2G |\
-bgzip -@ 8 > ${merged_SVs}/truvari.svimASM.vcf.gz
+-i ${merged_SVs}/merge.asm-2.sort.vcf.gz \
+-c ${merged_SVs}/truvari.asm-2.collapse.vcf.gz -f ${ref_genome} |\
+bcftools sort --max-mem 4G |\
+bgzip -@ ${nT} > ${merged_SVs}/truvari.asm-2.vcf.gz
 
-bcftools index -f -t ${merged_SVs}/truvari.svimASM.vcf.gz
+bcftools index -f -t ${merged_SVs}/truvari.asm-2.vcf.gz
 
 module unload python/3.10.2
 echo "This is the end!"
