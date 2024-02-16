23106c46d6e36629a342ce6a933c46a3c817136f
jenyuw <jenyuw@uci.edu>
2024-02-16 14:58:38 -0800

    new polarizing

--- hpc3/59_hpc3_asm-polarizing.sh
+++ hpc3/59_hpc3_asm-polarizing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=map    ## Name of the job.
+#SBATCH --job-name=pol    ## Name of the job.
 #SBATCH -A jje_lab       ## account to charge
 #SBATCH -p standard       ## partition/queue name
 #SBATCH --array=1      ## number of tasks to launch (wc -l prefixes.txt)
@@ -14,10 +14,11 @@ SVs="/dfs7/jje/jenyuw/SV-project-temp/result/SVs"
 merged_SVs="/dfs7/jje/jenyuw/SV-project-temp/result/merged_SVs"
 con_SVs="/dfs7/jje/jenyuw/SV-project-temp/result/consensus_SVs"
 nT=$SLURM_CPUS_PER_TASK
-
+source ~/.bashrc
 ## In interactive mode
-cd /dfs7/jje/jenyuw/SV-project-temp/result/polarizing
 
+
+: <<'SKIP'
 nucmer -t ${nT} --maxmatch -l 100 -c 500 --sam-long=${polarizing}/mumm4.sam ${dmel_ref} ${dsim_ref}
 cat ${polarizing}/mumm4.sam |\
 sed 's/HD\ /HD/; s/1.0\ /1.0/; s/\tSO:coordinate/SO:coordinate/; s/VN1/VN:1/; s/HD/HD\t/; s/SO:unsorted/\tSO:unsorted/; s/@PG /@PG\t/; s/ PN/\tPN/; s/ VN/\tVN/; s/ CL/\tCL/' |\
@@ -31,15 +32,20 @@ samtools index -@ ${nT} ${polarizing}/corrected.mumm4.bam
 #conda deactivate
 #svmu ${polarizing}/mumm4.delta ${dmel_ref} ${dsim_ref} l sam_lastz.txt svmu > svmu.tsv
 #Does not work well
+SKIP
+
+cd ${polarizing}
 
 bash /pub/jenyuw/Software/MUMandCo-MUMandCov3.8/mumandco_v3.8.sh  \
 -r ${dmel_ref} -q ${dsim_ref} \
--g 135000000 -o mumco -t ${nT} -ml 50
+-g 135000000 -o dmel-dsim_mumco -t ${nT} -ml 50
+
+cd ${polarizing}/dmel-dsim_mumco
 
 ## minimap2-asm20
 minimap2 -a -x asm20 -t ${nT} ${dmel_ref} ${dsim_ref} |\
-samtools sort -@ ${nT} -O bam -o ${polarizing}/mm2.bam
-samtools index -@ ${nT} ${polarizing}/mm2.bam
+samtools sort -@ ${nT} -O bam -o ${polarizing}/mm2-20.bam
+samtools index -@ ${nT} ${polarizing}/mm2-20.bam
 ## minimap2-asm10
 minimap2 -a -x asm10 -t ${nT} ${dmel_ref} ${dsim_ref} |\
 samtools sort -@ ${nT} -O bam -o ${polarizing}/mm2-10.bam
@@ -49,18 +55,23 @@ minimap2 -a -x asm10 -t ${nT} ${dmel_ref} ${dsim_ref} |\
 samtools sort -@ ${nT} -O bam -o ${polarizing}/mm2-5.bam
 samtools index -@ ${nT} ${polarizing}/mm2-5.bam
 
-
+: <<'SKIP'
 conda activate sv-calling
 lra global -CONTIG ${dmel_ref}
 lra align -CONTIG --refineBreakpoints ${dmel_ref} ${dsim_ref} -t 16 -p s |
 samtools sort -@ ${nT} -O bam -o ${polarizing}/lra.bam
 samtools index -@ ${nT} ${polarizing}/lra.bam
 conda deactivate
+svim-asm haploid --sample "sim2mel_lra_svimASM" --min_sv_size 50 \
+${polarizing}/lra_svimASM ${polarizing}/lra.bam ${dmel_ref}
 
+svim-asm haploid --sample "sim2mel_mumm4_svimASM" --min_sv_size 50 \
+${polarizing}/mumm4_svimASM ${polarizing}/corrected.mumm4.bam ${dmel_ref}
+SKIP
 
 conda activate sv-calling
 svim-asm haploid --sample "sim2mel_mm2_svimASM" --min_sv_size 50 \
-${polarizing}/mm2_svimASM ${polarizing}/mm2.bam ${dmel_ref}
+${polarizing}/mm2_svimASM ${polarizing}/mm2-20.bam ${dmel_ref}
 
 svim-asm haploid --sample "sim2mel_mm2-10_svimASM" --min_sv_size 50 \
 ${polarizing}/mm2-10_svimASM ${polarizing}/mm2-10.bam ${dmel_ref}
@@ -68,13 +79,6 @@ ${polarizing}/mm2-10_svimASM ${polarizing}/mm2-10.bam ${dmel_ref}
 svim-asm haploid --sample "sim2mel_mm2-5_svimASM" --min_sv_size 50 \
 ${polarizing}/mm2-5_svimASM ${polarizing}/mm2-5.bam ${dmel_ref}
 
-svim-asm haploid --sample "sim2mel_lra_svimASM" --min_sv_size 50 \
-${polarizing}/lra_svimASM ${polarizing}/lra.bam ${dmel_ref}
-
-svim-asm haploid --sample "sim2mel_mumm4_svimASM" --min_sv_size 50 \
-${polarizing}/mumm4_svimASM ${polarizing}/corrected.mumm4.bam ${dmel_ref}
-
-mv ${SVs}/${name}_svimASM/variants.vcf ${SVs}/${name}.svimASM.vcf 
 conda deactivate
 
 ## We choose the SV called by minimap2-asm20 and svim-asm
