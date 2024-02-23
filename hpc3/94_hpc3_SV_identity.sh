2b6b32bfd6860a4c12ba5bd02a387d0536df6357
jenyuw <jenyuw@uci.edu>
2024-02-23 13:48:43 -0800

    refine table

--- hpc3/94_hpc3_SV_identity.sh
+++ hpc3/94_hpc3_SV_identity.sh
@@ -66,14 +66,48 @@ ${TE}/SV.fa
 
 ##CHROM  POS     ID      REF     ALT     QUAL    FILTER  INFO    FORMAT  sample
 
-head -n 100 ${TE}/extracted_SV2/SV.fa.out|gawk ' NR > 3 {print $0}' | while read line
+cat ${TE}/extracted_SV2/SV.fa.out|gawk ' NR > 3 {print $0}' | while read line
 do
 chr_pos_id=`echo $line|gawk '{print $5}'|gawk -F "-" '{print $1 "\t" $2 "\t" $0 "\t"}'`
 order_supfam=`echo $line|gawk '{ if ( $11=="Simple_repeat") print $11 "\t" $10; else print $11}'|sed 's/\//\t/g;s/(//g;s/)n//g'`
 echo -e "${chr_pos_id}${order_supfam}"
 done >${TE}/SV_info.tsv
 
+tabix -f -s 1 -b 2 -e 2 fake.info.tsv.gz
 
+
+
+echo -e "##INFO=<ID=ORDER,Number=1,Type=String,Description=\"The order of the transposable element or a simple repeat\">
+##INFO=<ID=SUPERFAMILY,Number=1,Type=String,Description=\"The SUPERFAMILY of the transposable element or the motif of a simple repeat\">" \
+>${TE}/add_header.txt
+
+zcat ${polarizing}/corrected.polarized.asm.vcf.gz| bcftools annotate -a ${TE}/SV_info.tsv.gz -c 'CHROM,POS,ID,INFO/ORDER,INFO/SUPERFAMILY' --header-lines ${TE}/add_header.txt|less -S
+
+zcat ${polarizing}/corrected.polarized.asm.vcf.gz|head -n 10000| bcftools annotate -a ${TE}/fake.info.tsv.gz -c 'CHROM,POS,ID,INFO/ORDER,INFO/SUPERFAMILY' --header-lines ${TE}/add_header.txt|less -S
+
+#1.CHROM   2.POS     3.ID 4.ORDER 5.SUPERFAMILY 
+#1.CHROM    2.POS 3.ID [ %GT]
+##split the VCF
+bcftools query -f '%CHROM\t%POS\t%ID[ %GT]\n' ${polarizing}/corrected.polarized.asm.vcf.gz >${TE}/SV_genotype.tsv
+join -1 3 -2 3  <(sort -k 3,3 ${TE}/SV_info.tsv) <(sort -k 3,3 ${TE}/SV_genotype.tsv)|cut -d " " -f 2,3,1,4,5,8- |tr " " "\t" >${TE}/SV_genotype_info.tsv
+
+module load R/4.2.2
+Rscript -e 'library(tidyverse)
+t=read.table("fake.info.tsv",header=FALSE)
+colnames(t) <- c("CHROM","POS","ID","ORDER","SUPERFAMILY")
+t1=t %>% group_by_at(vars(SUPERFAMILY)) 
+head(t1,20) '
+%>%  summarize_all(paste, collapse=",")
+
+Rscript -e 't=read.table("fake.info.tsv",header=FALSE)
+colnames(t) <- c("CHROM","POS","ID","ORDER","SUPERFAMILY")
+t1 <- aggregate(cbind(ORDER, SUPERFAMILY)~., t,FUN = toString)
+head(t1,20) '
+tapply()
+aggregate()
+
+
+: <<'SKIP'
 cat ${TE}/extracted_SV2/SV.fa.out|gawk ' NR > 3 {print $0}' | while read line
 do
 chr_pos_id=`echo $line|gawk '{print $5}'|gawk -F "-" '{print $1 "\t" $2 "\t" $0 "\t"}'`
@@ -90,14 +124,4 @@ echo -e "##INFO=<ID=ORDER,Number=1,Type=String,Description=\"The order of the tr
 zcat ${polarizing}/corrected.polarized.asm.vcf.gz|grep "#"|grep -v "##"|gawk 'BEGIN { OFS = "\t"} {print $1, $2, $3, $4, $5, $6, $7, $8, $9, "sample" }' >>${TE}/new_header.txt
 ##Keep failing parsing the new VCF
 cat ${TE}/new_header.txt ${TE}/newbody.tsv |bcftools sort -O v >${TE}/annotation.vcf
-
-echo -e "##INFO=<ID=ORDER,Number=1,Type=String,Description=\"The order of the transposable element or a simple repeat\">
-##INFO=<ID=SUPERFAMILY,Number=1,Type=String,Description=\"The SUPERFAMILY of the transposable element or the motif of a simple repeat\">" \
->>${TE}/add_header.txt
-zcat ${polarizing}/corrected.polarized.asm.vcf.gz| bcftools annotate -a ${TE}/SV_info.tsv.gz -c 'CHROM,POS,ID,INFO/ORDER,INFO/SUPERFAMILY' --header-lines ${TE}/add_header.txt|less -S
-
-#1.CHROM   2.POS     3.ID 4.ORDER 5.SUPERFAMILY 
-#1.CHROM    2.POS 3.ID [ %GT]
-##split the VCF
-bcftools query -f '%CHROM\t%POS\t%ID[ %GT]\n' ${polarizing}/corrected.polarized.asm.vcf.gz >${TE}/SV_genotype.tsv
-join -1 3 -2 3  <(sort -k 3,3 ${TE}/SV_info.tsv) <(sort -k 3,3 ${TE}/SV_genotype.tsv)|cut -d " " -f 2,3,1,4,5,8- |tr " " "\t" >${TE}/SV_genotype_info.tsv
+SKIP
